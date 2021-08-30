555dc4467b78177fd1c6650c880facb9b34e95b1
Brian Kelleher <brian.kelleher@uconn.edu>
2021-08-30 12:46:21 -0600

    display the image property if it exists

--- a2a/src/components/MapListItem.vue
+++ a2a/src/components/MapListItem.vue
@@ -6,7 +6,13 @@
         <v-list-item-avatar
             tile
             color="grey"
-        ></v-list-item-avatar>
+        >
+            <v-img
+                v-if="waypoint.image"
+                :src="waypoint.image"
+                :alt="'Photo of ' + waypoint.name"
+            ></v-img>
+        </v-list-item-avatar>
         <v-list-item-content>
             <v-list-item-title class="subtitle-1">{{ waypoint.name }}</v-list-item-title>
             <v-list-item-subtitle>{{ address }}</v-list-item-subtitle>
