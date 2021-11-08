b211618135245e932d1d01c9fe14b9dfded29dc1
Brian Kelleher <brian.kelleher@uconn.edu>
2021-11-08 11:50:36 -0700

    pwa start

--- a2a/src/main.js
+++ a2a/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
