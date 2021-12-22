b2263bad9844c35b825fbd5a2518e143e37170f7
Brian Kelleher <brian.kelleher@uconn.edu>
2021-12-22 11:15:05 -0500

    wip, route list and router

---
+++ a2a/src/views/Routes.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="page-routes">
+        <div class="main-content">
+            <h1>Routes</h1>
+
+            <v-list>
+                <v-list-item
+                    v-for="r in routes"
+                    :key="`rlist-${r.id}`"
+                    @click="$router.push('map')"
+                >
+                    <v-list-item-content>
+                        {{ r.name }}
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+
+            {{ routes }}
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    data: () => {
+        return {}
+    },
+    computed: {
+        ...mapGetters({
+            routes: 'routeObjects'
+        })
+    }
+}
+</script>
